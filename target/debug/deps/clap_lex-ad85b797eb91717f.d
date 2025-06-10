c635da8533988800533fb53634d10e0a798fc3b1
Tito Barros Sala <titobsala@exo-team.com>
2025-06-10 22:10:58 +0100

    Add comprehensive .gitignore and remove target directory from tracking

--- target/debug/deps/clap_lex-ad85b797eb91717f.d
+++
@@ -1,8 +0,0 @@
-/home/tito-sala/Code/Personal/Raska/target/debug/deps/libclap_lex-ad85b797eb91717f.rmeta: /home/tito-sala/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-0.7.4/src/lib.rs /home/tito-sala/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-0.7.4/src/ext.rs
-
-/home/tito-sala/Code/Personal/Raska/target/debug/deps/libclap_lex-ad85b797eb91717f.rlib: /home/tito-sala/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-0.7.4/src/lib.rs /home/tito-sala/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-0.7.4/src/ext.rs
-
-/home/tito-sala/Code/Personal/Raska/target/debug/deps/clap_lex-ad85b797eb91717f.d: /home/tito-sala/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-0.7.4/src/lib.rs /home/tito-sala/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-0.7.4/src/ext.rs
-
-/home/tito-sala/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-0.7.4/src/lib.rs:
-/home/tito-sala/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-0.7.4/src/ext.rs:
